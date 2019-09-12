dae35cd909377431dc7c6fc2530b2da64ad5d58e
Raphael Pigulla <raphael.pigulla@maibornwolff.de>
2019-09-12 08:26:21 +0200

    initial checkin

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import nock from 'nock'
+import axios, { AxiosAdapter } from 'axios'
+
+// @ts-ignore
+import httpHdapter from 'axios/lib/adapters/http'
+
+import NodeEnvironment from 'jest-environment-node'
+
+export default class NockAxiosEnvironment extends NodeEnvironment {
+  private axiosAdapter: AxiosAdapter | undefined = undefined
+
+  public async setup (): Promise<void> {
+    await super.setup()
+
+    this.axiosAdapter = axios.defaults.adapter
+    axios.defaults.adapter = httpHdapter
+
+    nock.disableNetConnect()
+  }
+
+  public async teardown (): Promise<void> {
+    nock.cleanAll()
+    nock.enableNetConnect()
+
+    axios.defaults.adapter = this.axiosAdapter
+
+    await super.teardown()
+  }
+}
